17102e0b3f6542f7b64900f05feffb55f134b3c5
William Wallace <william.wallace@wcc.govt.nz>
2021-01-14 22:56:31 +1300

    first commit

---
+++ about.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+   <!-- Basic -->
+   <meta charset="utf-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <!-- Mobile Metas -->
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   <!-- Site Metas -->
+   <title>ABOUT | William Wallace</title>
+   
+   <?php include('includes/header.php') ?>
+      
+     <!-- about section -->
+     <section>
+      <div class="container">
+         <div class="row">
+            <div class="col-lg-12 text_align_center">
+               <div class="full">
+                  
+                  <p class="large2">I'm William Wallace, a developer from Wellington, New Zealand. 
+                     <br><br>
+                     I studied Computer Science and Neuroscience at The University of Otago, and currently I’m doing a Masters 
+                     in Computer Science at Victoria University of Wellington.   
+                     <br><br>
+                     I like working in various development fields, and now I'm focussing on databases, backend, and web development.    
+                  </p>
+               
+                  <u class="resume"><a  href="https://www.dropbox.com/scl/fi/os6934qa9jklaywxymiwn/William-Wallace-CV-December-2020.docx?dl=0&rlkey=zc0srrbtgvvajlhb7gc6jczue">Download Resume</a></u>
+               
+               </div>
+            </div>
+         </div>
+      </div>
+   </section>
+      <!-- end about section -->
+
+      
+      <?php include('includes/footer.php') ?>
---
+++ design.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+   <!-- Basic -->
+   <meta charset="utf-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <!-- Mobile Metas -->
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   <!-- Site Metas -->
+   <title>DESIGN | William Wallace</title>
+   
+   <?php include('includes/header.php') ?>
+    
+     <!-- about section -->
+     <section>
+      <div class="container">
+         <div class="row">
+            <div class="col-lg-12 text_align_center">
+               <div class="full large2">
+                  <p class="large2center">Design</h3>
+                  <p class="large"> Here are some of my renders, using Blender.
+                     <br><br>
+
+                  <img src="renders/chair-loop.gif" class="img-responsive" height="500px" width="500px"/> 
+                  <img src="renders/doughnut_forward_trim.gif" class="img-responsive" height="500px" width="500px"/> 
+               
+                  <input type="image" src="renders/scene.png" class="img-responsive" width="1000px" onclick="location.href='visual_art.php';" />
+                  <input type="image" src="renders/CoffeCup5.png" class="img-responsive" width="1000px" onclick="location.href='visual_art.php';" />
+                  <input type="image" src="renders/doughnut.png" class="img-responsive" width="1000px" onclick="location.href='visual_art.php';" />
+                  <!--<input type="image" src="renders/PenRendeWithPaperWideAngle.PNG" class="img-responsive"  width="1000px"onclick="location.href='blog.php';" />  -->
+               </div>
+            </div>
+         </div>
+         </div>
+      </div>
+   </section>
+   <!--end section -->
+     
+
+      
+   <?php include('includes/footer.php') ?>
